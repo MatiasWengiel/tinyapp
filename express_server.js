539389108434bef398971b82b36249c2acb2f178
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-27 16:33:47 -0500

    Adds the ability to redirect to the longURL by going to /u/shortURL

--- express_server.js
+++ express_server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const generateRandomString = () => {
+  let randomOutput = Math.random().toString(36) //Generates a pseudo-random number and turns it into a string
+  return randomOutput.substring(2, 7); //Returns five characters from the middle of the string for increased randomization
+}
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -38,15 +43,17 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n')
 })
 
-app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send('ok');
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(302, longURL)
 })
 
-const generateRandomString = () => {
-  let randomOutput = Math.random().toString(36) //Generates a pseudo-random number and turns it into a string
-  return randomOutput.substring(2, 7); //Returns five characters from the middle of the string for increased randomization
-}
+app.post('/urls', (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL
+
+  res.send('ok');
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
