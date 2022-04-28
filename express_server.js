d2652bbe9c6daf9fe7bfa444bf4a48efbb11068a
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-28 16:37:37 -0500

    Fix linting errors

--- express_server.js
+++ express_server.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = 8080;
 
 const generateRandomString = () => {
-  let randomOutput = Math.random().toString(36) //Generates a pseudo-random number and turns it into a string
+  let randomOutput = Math.random().toString(36); //Generates a pseudo-random number and turns it into a string
   return randomOutput.substring(2, 7); //Returns five characters from the middle of the string for increased randomization
-}
+};
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -24,50 +24,50 @@ app.get('/', (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-})
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
-})
+});
 
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { urls: urlDatabase, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render('urls_show', templateVars)
-})
+  res.render('urls_show', templateVars);
+});
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
-})
+});
 
 app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n')
-})
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
 
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(302, longURL)
-})
+  res.redirect(302, longURL);
+});
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect(302, "/urls");
-})
+});
 
 app.post('/urls/:shortURL/edit', (req, res) => {
   let newURL = req.body.newURL;
   if (newURL.substring(0,6) !== "http://" || newURL.substring(0,7) !== "https://") {
     newURL = "http://" + newURL;
   }
-  urlDatabase[req.params.shortURL] = newURL
-  res.redirect(302, '/urls')
-})
+  urlDatabase[req.params.shortURL] = newURL;
+  res.redirect(302, '/urls');
+});
 
 app.post('/urls', (req, res) => {
   const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL
+  urlDatabase[shortURL] = req.body.longURL;
 
   res.send('ok');
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
